4232848d838a34cd6285fd753a99afc17a4061b4
spotco <mootothemax@gmail.com>
2015-08-21 23:24:10 -0700

    GenericFootballer

--- Assets/Scripts/GenericFootballer.cs
+++ Assets/Scripts/GenericFootballer.cs
@@ -2,32 +2,24 @@
 using System.Collections;
 
 public class GenericFootballer : MonoBehaviour {
+
+	enum GenericFootballerMode {
+		Idle,
+		CommandMoving
+	}
+
 	[SerializeField] private float _moveSpeed = 10.0f;
 	
-	void Update () {
-		Vector3 moveVec = Vector3.zero;
-		if(Input.GetKey(KeyCode.UpArrow)) {
-			moveVec += Vector3.up * Time.deltaTime * _moveSpeed;
-		}
-		if(Input.GetKey(KeyCode.DownArrow)) {
-			moveVec -= Vector3.up * Time.deltaTime * _moveSpeed;
-		}
-		if(Input.GetKey(KeyCode.RightArrow)) {
-			moveVec += Vector3.right * Time.deltaTime * _moveSpeed;
-		}
-		if(Input.GetKey(KeyCode.LeftArrow)) {
-			moveVec -= Vector3.right * Time.deltaTime * _moveSpeed;
-		}
-		
-		// TODO: omg
-		if (!Mathf.Approximately(moveVec.magnitude, 0.0f)) {
-			float rad = 25.0f * 0.5f;
-			Vector3 newPos = transform.localPosition + moveVec * rad;
-			bool coll = Physics2D.Linecast(transform.localPosition, newPos, 1 << LayerMask.NameToLayer("Ground"));  
-			
-			if (!coll) {
-				transform.localPosition += moveVec;
-			}
-		}
+	public void sim_initialize() {
+
 	}
+
+	public void sim_update(float dt_scale) {
+
+	}
+
+	public void CommandMoveTo(Vector2 pos) {
+
+	}
+
 }
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,17 +4,23 @@ using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour {
 
+	enum LevelControllerMode {
+		GamePlay,
+		Timeout
+	}
+
 	[SerializeField] private GameObject proto_genericFootballer;
 
 	private PathRenderer m_pathRenderer;
 	private List<GenericFootballer> m_allFootballers = new List<GenericFootballer>();
+	private LevelControllerMode m_currentMode;
 
 	public void StartLevel() {
 		m_pathRenderer = this.GetComponent<PathRenderer>();
 		m_pathRenderer.id_draw_path(0,new Vector3(0,0,0),new Vector3[]{ new Vector3(200,200,200) });
 
 		m_allFootballers.Add(this.CreateFootballer(new Vector3(0,0)));
-
+		m_currentMode = LevelControllerMode.GamePlay;
 	}
 
 	private GenericFootballer CreateFootballer(Vector3 pos) {
@@ -25,6 +31,20 @@ public class LevelController : MonoBehaviour {
 	}
 
 	void Update () {
+		float dt_scale = (1/60.0f)/(Time.deltaTime);
+		switch(m_currentMode) {
+		case LevelControllerMode.GamePlay: {
+			for (int i = 0; i < this.m_allFootballers.Count; i++) {
+				GenericFootballer itr = this.m_allFootballers[i];	
+				itr.sim_update(dt_scale);
+			}
+
+		} break;
+		case LevelControllerMode.Timeout: {
+
+
+		} break;
+		}
 
 	}
 
